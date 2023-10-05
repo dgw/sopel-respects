c147213d495bafcdd9ef540722028da8183fb2de
xnaas <me@xnaas.info>
2023-10-05 16:16:57 -0500

    ' => "

--- f.py
+++ f.py
@@ -1,11 +1,10 @@
 """
 Authors: xnaas <me@xnaas.info> (2022)
 License: The Unlicense (public domain)
-Require: python>=3.8
 """
 from sopel import plugin
 
 
-@plugin.rule('^F$')
+@plugin.rule(r"^F$")
 def pay_respects(bot, trigger):
-	bot.action('pays respects')
+	bot.action("pays respects")
